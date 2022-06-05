174771e70894761a5558ef1743e04b425b0c550c
victor <victoragustdp@gmail.com>
2022-06-04 23:57:35 -0300

    feat: Criando entidade Pet

---
+++ src/main/java/br/com/adopt/petsback/domain/entity/Pet.java
@@ -0,0 +1,32 @@
+package br.com.adopt.petsback.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pet {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String history;
+
+    @Column(nullable = false)
+    private String photo;
+
+}
