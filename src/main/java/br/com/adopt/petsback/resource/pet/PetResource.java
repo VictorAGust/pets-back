c13ccad4f84cea1dbf7420a0c90ccb52ce5a7077
victor <victoragustdp@gmail.com>
2022-06-05 00:01:00 -0300

    feat: Criando Resource para Pet

---
+++ src/main/java/br/com/adopt/petsback/resource/pet/PetResource.java
@@ -0,0 +1,30 @@
+package br.com.adopt.petsback.resource.pet;
+
+
+import br.com.adopt.petsback.domain.dto.PetDto;
+import br.com.adopt.petsback.service.PetService;
+import br.com.adopt.petsback.service.adapter.PetMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/pets")
+@RestController
+public class PetResource {
+
+    private final PetService petService;
+
+    private final PetMapper petMapper;
+
+    @GetMapping("/")
+    public ResponseEntity<List<PetDto>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(petService.findAll());
+    }
+}
