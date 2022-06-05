34d964f4ef0dddc16e31c57f38967ad682ba3f75
victor <victoragustdp@gmail.com>
2022-06-04 23:58:37 -0300

    feat: Criando DTO de pet

---
+++ src/main/java/br/com/adopt/petsback/domain/dto/PetDto.java
@@ -0,0 +1,16 @@
+package br.com.adopt.petsback.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PetDto {
+
+    private Long id;
+    private String name;
+    private String history;
+    private String photo;
+}
