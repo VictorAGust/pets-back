1085a2d1a53e502ec6c910926e8486b6bd76a6bb
victor <victoragustdp@gmail.com>
2022-06-05 00:03:15 -0300

    chore: Criando massa de dados para insert ao inicio da aplicação

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO) VALUES ('1', 'bilu', 'lala', 'https://razoesparaacreditar.com/wp-content/uploads/2021/05/cachorro-deformidades-1.jpg');
+INSERT INTO PET (ID, NAME, HISTORY, PHOTO) VALUES ('2', 'curumin', 'lolo', 'https://img.r7.com/images/cachorro-que-parece-pompom-13012022171412951?dimensions=771x420');
