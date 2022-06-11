16b4b7ae5c01505f8397690c763ca12300ef67ec
victor <victoragustdp@gmail.com>
2022-06-10 23:11:05 -0300

    refactor: Refatorando FindByIdOrElseThrow em PetRepository

--- src/main/java/br/com/adopt/petsback/repository/PetRepository.java
+++ src/main/java/br/com/adopt/petsback/repository/PetRepository.java
@@ -1,20 +1,13 @@
 package br.com.adopt.petsback.repository;
 
 import br.com.adopt.petsback.domain.entity.Pet;
+import br.com.adopt.petsback.exception.PetNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
-    default Pet findByIdOrElseThrow(Long id){
-        var petOptional = findById(id);
-
-        if(petOptional.isPresent()) {
-            return petOptional.get();
-        } else {
-            throw new EntityNotFoundException("Pet not found");
-        }
+    default Pet findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(PetNotFoundException::new);
     }
 }
