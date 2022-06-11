2e5d80807819450aad637aa97bece0d82a6b4dcb
victor <victoragustdp@gmail.com>
2022-06-10 22:55:04 -0300

    feat: Adicionando validação de dados

--- src/main/java/br/com/adopt/petsback/domain/dto/AdoptionRequestDto.java
+++ src/main/java/br/com/adopt/petsback/domain/dto/AdoptionRequestDto.java
@@ -1,12 +1,14 @@
 package br.com.adopt.petsback.domain.dto;
 
+import br.com.adopt.petsback.validator.PetExistsById;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 @Data
@@ -15,7 +17,19 @@ import java.math.BigDecimal;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AdoptionRequestDto {
 
-    private BigDecimal value;
+    @NotNull
+    @Email
+    @NotEmpty
+    @Size(max = 255)
     private String email;
+
+    @NotNull
+    @Min(10)
+    @Max(100)
+    private BigDecimal value;
+
+    @NotNull
+    @Positive
+    @PetExistsById
     private Long petId;
 }
